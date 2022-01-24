466e9ddc48dad74d16d7ba91c015867b5a2f84b0
mtsamy333 <mtsamy333@gmail.com>
2022-01-24 15:03:23 +0530

    new commits

--- space/src/app/airplane/airplane.component.ts
+++ space/src/app/airplane/airplane.component.ts
@@ -18,38 +18,30 @@ export class AirplaneComponent implements OnInit {
       console.log("responce recived ",data),
       this.flightData = data;
       if (this.flightData.length == 0) {
-        // this.showError = "No Record Found";
       }
       console.log("Data :", this.flightData)
-      // error=>console.log("exception recoved ")
     })
   }
   sendYear(header6:any): void {
     console.log(header6);
     this.http.fetchAll(header6, this.success, this.LandSucc).subscribe(data => {
-      // console.log("responce recived ",data),
       this.flightData = data;
       console.log("success :", this.flightData)
-      // error=>console.log("exception recoved ")
     })
   }
   sendSuccess(succ:any) {
     this.success = succ;
     console.log(succ);
     this.http.fetchLanchSucess(succ).subscribe(data => {
-      // console.log("responce recived ",data),
       this.flightData = data;
       console.log("sucees :", this.flightData)
-      // error=>console.log("exception recoved ")
     })
   }
   LandSuccLuanchSucc(val:any) {
     this.LandSucc = val;
     this.http.fetchLanchSucessAndLandSuccess(val).subscribe(data => {
-      // console.log("responce recived ",data),
       this.flightData = data;
       console.log("Land :", this.flightData)
-      // error=>console.log("exception recoved ")
     })
   }
 }
